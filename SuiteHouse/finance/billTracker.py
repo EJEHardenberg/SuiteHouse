a13c2deba594522bdc1b825de1a3a9426287ff10
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2013-06-15 15:49:09 -0400

    Add functionality for Bills for responding to PUT, POST, and DELETE request. Also added static method to aggregate all bills

--- SuiteHouse/finance/billTracker.py
+++ SuiteHouse/finance/billTracker.py
@@ -48,6 +48,29 @@ class BillItem(baseItem.BaseItem,db.Model):
 import logging
 
 class BillTracker(webapp2.RequestHandler):
+	@staticmethod
+	def getTotalBills():
+		"""Returns the total bill expenses from the database in a key value pair
+			{'bills' : x } 
+		"""
+
+		user = users.get_current_user()
+
+		if user:
+			#Get all the items associated with the user
+			bills = BillItem.by_id_forMonth(user.nickname())
+
+			#aggregate data
+			runningBills = 0
+
+			for item in bills:
+				runningBills = runningBills + item.amount
+
+			return {'bills' : runningBills}
+
+
+		else:
+			self.redirect(users.create_login_url(self.request.uri))
 
 	def get(self):
 		user = users.get_current_user()
@@ -69,13 +92,86 @@ class BillTracker(webapp2.RequestHandler):
 
 	def post(self):
 		user = users.get_current_user()
+		desc = cgi.escape(self.request.get('description'))
+		amount = cgi.escape(self.request.get('amount'))
+		err = ""
+		
+		try:
+			amount = float(amount)
+		except Exception, e:
+			#They didn't submit a numeric integer (aka they just hit submit without entering a value)
+			err = "Please enter an amount for your item"
+			amount = 0
+			#Redirects here don't work for some reason
+			self.response.set_status(400,'Bad amount value')
+			self.response.write(err)
+			self.redirect('/finance/billTracker?err=%s' % cgi.escape(err))
+
+		#We negate the value because we don't except negative values in the html input form
+		amount = 0 - amount
+			
+
+		#Create the new item and store it in the database
+		if err == "":
+			newItem = BillItem(description=desc,amount=amount,associated_user=str(user.nickname()))
+			newItem.put()
+			time.sleep(1) # this is so that when we do the redirect (to essentially refresh the page), we ensure the datastore has been updated and we see a new value
+			# we should redirect the user here, otherwise we ham up the redirects
+			self.redirect('/finance/billTracker') 
 
 	def delete(self):
 		"""Respond to an http delete request, this will delete whatever BillItem the request specifies"""
 
-		pass
+		#TODO: Since we use the database key, we could probably just abstract this method to a higher class for each controller
+
+		#Get the key to the item
+		try:
+			body = json.loads((self.request.body))
+			key = body['key']
+			item = db.get(db.Key(encoded=str(key)))
+			item.delete()
+			self.response.set_status(200,json.dumps({'key' : key}))
+			self.response.write(key)
+		except Exception, e:
+			self.response.set_status(404,json.dumps({'key' : 'Not found'}))
+		else:
+			#Carry on my wayward (son
+			pass
 			
 	def put(self):
-		pass
+		#Since this retrieves via key, we could probably abstract a good portion of it out to a controller class... 
+		body = json.loads(self.request.body)
+		key = body['key']
+
+		amount = None
+		description = None
+
+		try:
+			amount = body['amount']
+		except Exception, e:
+			#no amount to have
+			pass
+
+		try:
+			description = body['description']
+		except Exception, e:
+			#no description to have
+			pass
+
+		#Did they submit something worthwhile?
+		if amount or description:
+			try:
+				item = db.get(db.Key(encoded=str(key)))
+				if amount:
+					item.amount = float(amount)
+				if description:
+					item.description = description
+				item.put()
+				self.response.set_status(200,json.dumps({'key' : key}))
+				self.response.write(key)
+			except Exception, e:
+				logging.info(e)
+				self.response.set_status(500,json.dumps({'error' : str(e)}))
+				self.response.write('Could not complete update')
 
 		
